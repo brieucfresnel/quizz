f617a8d72c12f95d0576837aec1c2eb22e7379ba
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2020-03-10 11:42:06 +0100

    add home css

--- front/src/App.js
+++ front/src/App.js
@@ -1,26 +1,88 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+import Quizz from './Quizz/Quizz';
+import QuizzThumbnail from './QuizzThumbnail/QuizzThumbnail';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    let [quizzes, setQuizzes] = useState([]);
+    let [quizzesThumbnails, setQuizzesThumbnails] = useState({});
+    let [currentCategory, setCurrentCategory] = useState('all');
+
+    async function getQuizzes() {
+        setQuizzes((await axios.get('http://localhost:8000/quizzes')).data);
+    }
+
+    async function addQuizz(q, images=false, video=false) {
+        if(images) {
+            // Handle image answers
+        }
+
+        if(video) {
+            // Handle video question
+        }
+
+        setQuizzes((await axios.post('http://localhost:8000/quizz', q)).data);
+        // Redirect to home
+    }
+
+    async function deleteQuizz(q) {
+        setQuizzes((await axios.delete('http://localhost:8000/quizz, q')).data);
+    }
+
+    let jsxQuizzes = {};
+    if(quizzesThumbnails.length != 0) {
+        if(!currentCategory !== 'all') {
+
+            // jsxQuizzes = quizzesThumbnails
+            //     .filter(quizzThumbnail => quizzThumbnail.category == currentCategory)
+            //     .map(quizzThumbnail =>
+            //         <QuizzThumbnail name={quizzThumbnail.name} questions={quizzThumbnail.questions}/>
+            //     );
+        } else {
+
+        }
+
+    }
+
+    useEffect(() => {
+        getQuizzes()
+    }, []);
+
+    jsxQuizzes = quizzes.map(quizz =>
+        <QuizzThumbnail
+            key={quizz.id}
+            name={quizz.name}
+            creator_id={quizz.creator_id}
+            category={quizz.category}
+            creation_date={quizz.creation_date}/>
+        );
+
+    return (
+    <div className="wrapper">
+        <header className="top-bar">
+            <h1>Graphizz!</h1>
+            <img className="icon profile-btn" src={process.env.PUBLIC_URL + "/img/icons/icon_user.png"}/>
+        </header>
+        <main className="quizzThumbnails">
+            {jsxQuizzes}
+            {jsxQuizzes}
+            {jsxQuizzes}
+        </main>
+
+        <div className="bot-bar">
+            <div className="control-btn">
+                <img className="control-btn-icon" src={process.env.PUBLIC_URL + "/img/icons/icon_add.png"}/>
+            </div>
+            <div className="control-btn">
+                <img className="control-btn-icon" src={process.env.PUBLIC_URL + "/img/icons/icon_search.png"}/>
+            </div>
+        </div>
     </div>
-  );
+
+    );
 }
 
 export default App;
