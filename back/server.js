63f51e5f282a0eec1917fa7abdb65f23c29d6e59
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2020-03-12 14:58:22 +0100

    fix quizzThumbnails dynamic bgImag

--- back/server.js
+++ back/server.js
@@ -6,13 +6,13 @@ const cors = require('cors');
 const morgan = require('morgan');
 const bodyParser = require("body-parser");
 
-app.use(express.static('/public'));
 
 app.use(morgan('combined')); // to get informations from requests
 app.use(cors()); // to allow resquets from another application
 app.use(bodyParser.urlencoded({
         extended: true
     }));
+    app.use(express.static('public/'));
 
 app.use(router) // Requests processing will be defined in the file router
     .listen(port, () => console.log('Server app listening on port ' + port));
