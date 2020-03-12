9bf3b99a0ef41c695b24ee5ffb67881cc6380b75
Brieuc Fresnel <fresnel.brieuc@gmail.com>
2020-03-12 11:23:19 +0100

    more on NewFormQuizz comp

---
+++ front/src/components/NewQuizzForm.js
@@ -0,0 +1,70 @@
+import React, {useState} from 'react';
+
+export default function NewQuizzForm(props) {
+
+    let [currPageTitle, setCurrPageTitle] = useState('quizzInfo');
+    let pageToShow = '';
+
+    switch(currPageTitle) {
+        case 'quizzInfo':
+            return (
+                <div className="newQuizzForm">
+                    <h1>Créez votre propre quizz</h1>
+                    <form>
+                        <input type="text" name="quizzName" required/>
+                        <input type="file" name="quizzImage" required/>
+                        <select name="quizzCategory" required>
+                            <option value="webdesign">
+                        </select>
+                    </form>
+                </div>
+            );
+            break;
+        case 'questionType':
+            // show questions if there are any + delete btn
+            // choose question type: video or text
+            return (
+                <div className="newQuizzForm">
+
+                </div>
+            );
+            break;
+        case 'textQuestion':
+            // input question, choose answer type
+            return (
+                <div className="newQuizzForm">
+
+                </div>
+            );
+            break;
+        case 'textQuestion_textAnswers':
+            // show question, fill answers, choose rightAnswer
+            return (
+                <div className="newQuizzForm">
+
+                </div>
+            );
+            break;
+        case 'textQuestion_imgAnswers':
+            // show question, upload images, choose rightAnswer
+            return (
+                <div className="newQuizzForm">
+
+                </div>
+            );
+            break;
+        case 'videoQuestion':
+            // upload video, fill questions, choose rightAnswer
+            return (
+                <div className="newQuizzForm">
+
+                </div>
+            );
+            break;
+    }
+    return (
+        <div className="newQuizzForm">
+            {jsxPageToShow}
+        </div>
+    );
+}
